dc7744ab00c4b0879b388345fc364be460322e62
unknown <guamok@hotmail.com>
2023-11-20 13:09:28 +0100

    first commit

---
+++ src/main/java/com/comercioelectronico/ComercioelectronicoApplication.java
@@ -0,0 +1,13 @@
+package com.comercioelectronico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComercioelectronicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComercioelectronicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comercioelectronico/ComercioelectronicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comercioelectronico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComercioelectronicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
